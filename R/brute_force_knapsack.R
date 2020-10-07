1213b3d57ae4e91348beae52e202a578bc77e22f
Alejo Perez Gomez <alejo.perez@estudiant.upc.edu>
2020-10-07 14:11:31 +0200

    round change

--- R/brute_force_knapsack.R
+++ R/brute_force_knapsack.R
@@ -1,7 +1,7 @@
 brute_force_knapsack <- function(x,W){
-  stopifnot("x is not a data frame object" =is.data.frame(x),
-            "W is not numeric" = is.numeric(W),
-            "x does not contain columns with names (w, v) " = colnames(x) %in% c("w","v") )
+  stopifnot("x is not a data frame object"=is.data.frame(x),
+            "W is not numeric"=is.numeric(W),
+            "x does not contain columns with names (w, v)"=colnames(x) %in% c("w","v"))
 
   # lets use all combinations
   AllCombinations <- data.frame(row.names = c("elements", "weights", "value"))
@@ -9,11 +9,11 @@ brute_force_knapsack <- function(x,W){
     WeightComb <- unname(colSums(combn(x$w, i)))
     ValueComb <-  unname(colSums(combn(x$v, i)))
     elementComb <- unname(as.list(as.data.frame(combn(row.names(x), i))))
-    new <- data.frame(I(elementComb), WeightComb, ValueComb)
-    AllCombinations <- rbind(AllCombinations,new)
+    new <- data.frame(I(elementComb), WeightComb, round(ValueComb))
+    AllCombinations <- rbind(AllCombinations, new)
   }
 
   filter <- AllCombinations[AllCombinations$WeightComb <= W,]
-  result <- filter[filter$ValueComb == max(filter$ValueComb),]
+  result <- filter[filter$round.ValueComb == max(filter$round.ValueComb),]
   return(result)
 }
