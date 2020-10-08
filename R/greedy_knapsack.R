9da10ae5902aef722061bccd16ed0309d3dc5b6d
Alejo Perez Gomez <alejo.perez@estudiant.upc.edu>
2020-10-08 14:26:19 +0200

    Greedy implemented

---
+++ R/greedy_knapsack.R
@@ -0,0 +1,58 @@
+greedy_knapsack <- function(x, W){
+  "Return maximum value of items and their fractional amounts.
+
+    (max_value, fractions) is returned where max_value is the maximum value of
+    items with total weight not more than capacity.
+    fractions is a list where fractions[i] is the fraction that should be taken
+    of item i, where 0 <= i < total number of items.
+
+    value[i] is the value of item i and weight[i] is the weight of item i
+    for 0 <= i < n where n is the number of items.
+
+    capacity is the maximum weight.
+    "
+  x$ratio <- x$v / x$w
+
+  sorted <- x[order(x$ratio, decreasing = TRUE),]
+
+
+  max_value <- 0
+  fractions <- array(dim=length(sorted$ratio))
+  for (row in 1:nrow(sorted)) {
+
+    if (sorted[row,'w'] <= W) {
+      fractions[row] <- 1
+      max_value <- max_value + sorted[row,'v']
+      W = W - sorted[row,'w']
+    }
+
+
+
+    else{
+      fractions[row] = W/sorted[row,'w']
+      max_value <- max_value + sorted[row,"v"]*W/sorted[row,'w']
+      break
+      }
+    }
+    return(list(max_value, fractions) )
+
+}
+
+a <- greedy_knapsack(x = knapsack_objects[1:4,], 2000)
+
+
+
+ # n = int(input('Enter number of items: '))
+ # value = input('Enter the values of the {} item(s) in order: '
+                #.format(n)).split()
+  # = [int(v) for v in value]
+  #weight = input('Enter the positive weights of the {} item(s) in order: '
+                 #.format(n)).split()
+  #weight = [int(w) for w in weight]
+  #capacity = int(input('Enter maximum weight: '))
+
+  #max_value, fractions = fractional_knapsack(value, weight, capacity)
+  #print('The maximum value of items that can be carried:', max_value)
+  #print('The fractions in which the items should be taken:', fractions)
+
+
