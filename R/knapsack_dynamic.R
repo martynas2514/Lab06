423d5b6aad484d079f9b93d1293826274563c3d9
Martynas Lukosevicius <martynas2514@gmail.com>
2020-10-08 19:38:11 +0200

    dynamoc

--- R/knapsack_dynamic.R
+++ R/knapsack_dynamic.R
@@ -1,10 +1,20 @@
-library(Rcpp)
+#' knapsack_dynamic
+#'
+#' @param x dataframe
+#' @param W integer
+#'
+#' @return list
+#' @import Rcpp
+#' @export
+
 
 
 knapsack_dynamic <- function(x, W){
   value <- matrix(-1, nrow = length(x$w) + 1, ncol = W +1)
   value[1, ] <- 0
   value[ , 1] <- 0
+
+
 
   m <- function(i,j){
 
@@ -16,19 +26,22 @@ knapsack_dynamic <- function(x, W){
       value[i-1, j] <<-  m(i-1,j)
     }
 
-    weightToCheck <- j - x$w[i - 1] # negative means too heavy
-    if(x$w[i - 1] > j || weightToCheck <= 0 ){
+    if(x$w[i - 1] > j || j - x$w[i - 1] <= 0 ){
       value[i, j] <<-  value[i-1, j]
     }
     else{
 
-      if(value[i, weightToCheck] == -1){
-        value[i, weightToCheck] <<-  m(i, weightToCheck)}
+      if(value[i, j - x$w[i - 1]] == -1){
+        value[i, j - x$w[i - 1]] <<-  m(i, j - x$w[i - 1])}
 
       value[i, j] <<- max(value[i-1, j], value[i -1, j - x$w[i - 1]] + x$v[i - 1])
     }
   }
+
+
+
  result <-  m(length(x$w)+1, W+1)
+# result <-  mCpp(length(x$w), W , value, x)
 
 
 
@@ -45,7 +58,6 @@ knapsack_dynamic <- function(x, W){
    }
    i = i-1
  }
-
  return(list(value = result , elements = items))
 }
 
