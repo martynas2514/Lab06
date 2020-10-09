88a043f763964ad0456449f62c048b989a1c30db
Alejo Perez Gomez <alejo.perez@estudiant.upc.edu>
2020-10-09 11:20:34 +0200

    v1

--- R/greedy_knapsack.R
+++ R/greedy_knapsack.R
@@ -40,21 +40,4 @@ greedy_knapsack <- function(x, W){
 
 }
 
-a <- greedy_knapsack(x = knapsack_objects[1:4,], 2000)
-
-
-
- # n = int(input('Enter number of items: '))
- # value = input('Enter the values of the {} item(s) in order: '
-                #.format(n)).split()
-  # = [int(v) for v in value]
-  #weight = input('Enter the positive weights of the {} item(s) in order: '
-                 #.format(n)).split()
-  #weight = [int(w) for w in weight]
-  #capacity = int(input('Enter maximum weight: '))
-
-  #max_value, fractions = fractional_knapsack(value, weight, capacity)
-  #print('The maximum value of items that can be carried:', max_value)
-  #print('The fractions in which the items should be taken:', fractions)
-
 
