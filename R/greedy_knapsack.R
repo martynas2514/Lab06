9a584969792b6c0c149bb3c24448ae6b9ae9dddd
Alejo Perez Gomez <alejo.perez@estudiant.upc.edu>
2020-10-09 11:21:42 +0200

    v1

--- R/greedy_knapsack.R
+++ R/greedy_knapsack.R
@@ -1,9 +1,9 @@
 greedy_knapsack <- function(x, W){
   "Return maximum value of items and their fractional amounts.
 
-    (max_value, fractions) is returned where max_value is the maximum value of
+    (MaxValue, Fractions) is returned where MaxValue is the maximum value of
     items with total weight not more than capacity.
-    fractions is a list where fractions[i] is the fraction that should be taken
+    Fractions is a list where Fractions[i] is the fraction that should be taken
     of item i, where 0 <= i < total number of items.
 
     value[i] is the value of item i and weight[i] is the weight of item i
@@ -16,23 +16,23 @@ greedy_knapsack <- function(x, W){
   sorted <- x[order(x$ratio, decreasing = TRUE),]
 
 
-  max_value <- 0
-  fractions <- array(dim=length(sorted$ratio))
+  MaxValue <- 0
+  Fractions <- array(dim=length(sorted$ratio))
   for (row in 1:nrow(sorted)) {
     if (sorted[row,'w'] <= W) {
-      fractions[row] <- 1
-      max_value <- max_value + sorted[row,'v']
+      Fractions[row] <- 1
+      MaxValue <- MaxValue + sorted[row,'v']
       W = W - sorted[row,'w']
     }
 
 
     else{
-      fractions[row] <- W/sorted[row,'w']
-      max_value <- max_value + sorted[row,"v"]*W/sorted[row,'w']
+      Fractions[row] <- W/sorted[row,'w']
+      MaxValue <- MaxValue + sorted[row,"v"]*W/sorted[row,'w']
       break
       }
   }
-    indexes <- which(fractions==TRUE)
+    indexes <- which(Fractions==TRUE)
     whole_value <- round(sum(sorted[indexes,"v"]))
     elements <- unlist(lapply(rownames(sorted[indexes,]), as.integer))
 
